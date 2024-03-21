f6fa5e464fd192e5bcf503e764e5c92a5bb5f7f5
Mounika M <mounika17112003@gmail.com>
2024-03-21 13:22:24 +0530

    second commit

--- src/Securin.java
+++ src/Securin.java
@@ -4,25 +4,25 @@ public class Securin {
 
 
         public static void main(String[] args) {
-            int[] dieA = {1, 2, 3, 4, 5, 6};
-            int[] dieB = {1, 2, 3, 4, 5, 6};
+            int[] A = {1, 2, 3, 4, 5, 6};
+            int[] B = {1, 2, 3, 4, 5, 6};
 
-            int totalCombinations = countCombinations(dieA, dieB);
-            System.out.println("Total combinations: " + totalCombinations);
+            int total = count(A, B);
+            System.out.println("Total Combinations: " + total);
 
-            HashMap<Integer, Integer> distribution = calculateDistribution(dieA, dieB);
+            HashMap<Integer, Integer> dist = calculateDist(A, B);
 
-            int totalCombinations2 = dieA.length * dieB.length;
+            int totalCombinations2 = A.length * B.length;
 
-            System.out.println("Distribution of all possible combinations and their probabilities:");
-            for (int sum = 2; sum <= 12; sum++) {
-                int frequency = distribution.getOrDefault(sum, 0);
+            System.out.println("Distribution of all possible combinations & their probabilities:");
+            for (int i= 2; i <= 12; i++) {
+                int frequency = dist.getOrDefault(i, 0);
                 double probability = (double) frequency / totalCombinations2;
-                System.out.println("Sum " + sum + ": " + frequency + " occurrences, Probability: " + probability);
+                System.out.println("Sum " + i + ": " + frequency + " occurrences, Probability: " + probability);
             }
         }
 
-        public static int countCombinations(int[] dieA, int[] dieB) {
+        public static int count(int[] dieA, int[] dieB) {
             int count = 0;
             for (int i = 0; i < dieA.length; i++) {
                 for (int j = 0; j < dieB.length; j++) {
@@ -32,15 +32,15 @@ public class Securin {
             return count;
         }
 
-        public static HashMap<Integer, Integer> calculateDistribution(int[] dieA, int[] dieB) {
-            HashMap<Integer, Integer> distribution = new HashMap<>();
+        public static HashMap<Integer, Integer> calculateDist(int[] dieA, int[] dieB) {
+            HashMap<Integer, Integer> dist1 = new HashMap<>();
             for (int i = 0; i < dieA.length; i++) {
                 for (int j = 0; j < dieB.length; j++) {
                     int sum = dieA[i] + dieB[j];
-                    distribution.put(sum, distribution.getOrDefault(sum, 0) + 1);
+                    dist1.put(sum, dist1.getOrDefault(sum, 0) + 1);
                 }
             }
-            return distribution;
+            return dist1;
         }
     }
 
